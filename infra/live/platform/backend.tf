c353d6f9210b2be8004d1e0a5fb1612423c9979a
tahmidur55 <tahmidur55@gmail.com>
2025-12-29 18:01:44 +0000

    Updated backend.tf

--- infra/live/platform/backend.tf
+++ infra/live/platform/backend.tf
@@ -5,6 +5,5 @@ terraform {
     resource_group_name  = "sttfldatatfstate-rg"
     storage_account_name = "sttfldatatfstate"
     container_name       = "sttfldatatfstate-container"
-    use_oidc             = true
   }
 }
